b37b2d7afcc63cfa82528fb21d1d45aed665ac5a
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-12-02 00:18:40 +0500

    oxirgisi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "SoftwareSerial.h"
 #include "DS18B20.h"
+#include "SPIFFS.h"
 
 #define DALLAS_PIN  25
 #define MT3608_PIN  33
@@ -14,16 +15,34 @@
 #define RS485_RX_PIN  26
 #define RS485_TX_PIN  27
 
+#define uS_TO_S_FACTOR  1000000ULL
+
 OneWire oneWire(DALLAS_PIN);
 DS18B20 sensor(&oneWire);
+float getTemperature();
 
 void setup() {
+  pinMode(SIM_PWR_PIN, OUTPUT);
+  digitalWrite(SIM_PWR_PIN, 1);
+  delay(3000);
+  digitalWrite(SIM_PWR_PIN, 0);
+  Serial.begin(115200);
   sensor.begin();
   sensor.setResolution(10);
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    delay(5000);
+    esp_restart();
+  }
+  File file = SPIFFS.open("/sensor.temp", "r", true);
+  
 }
+uint32_t last_time = 0, time_span = 1000;
 
 void loop() {
 
+  Serial.printf("Temperature: %.2f\r\n", getTemperature());
+  esp_deep_sleep(10 * uS_TO_S_FACTOR);
 }
 
 float getTemperature() {
