281b6efbd0167c394a52700ee618938cffa65c74
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-12-01 13:13:37 +0500

    Add temperature sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include "SoftwareSerial.h"
+#include "DS18B20.h"
 
 #define DALLAS_PIN  25
 #define MT3608_PIN  33
@@ -12,8 +14,22 @@
 #define RS485_RX_PIN  26
 #define RS485_TX_PIN  27
 
+OneWire oneWire(DALLAS_PIN);
+DS18B20 sensor(&oneWire);
+
 void setup() {
+  sensor.begin();
+  sensor.setResolution(10);
 }
 
 void loop() {
+
+}
+
+float getTemperature() {
+  sensor.requestTemperatures();
+  while (!sensor.isConversionComplete()) {
+    delay(10);
+  }
+  return sensor.getTempC();
 }
