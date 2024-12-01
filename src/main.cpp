5b92ea691da21f6efbfd255a7872149186865735
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-12-01 13:07:31 +0500

    Defined pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+#define DALLAS_PIN  25
+#define MT3608_PIN  33
+#define VPP_SP_PIN  39
+#define VCCBAT_PIN  36
+
+#define SIM_PWR_PIN 5
+#define SIM_RX_PIN  18
+#define SIM_TX_PIN  19
+
+#define RS485_RX_PIN  26
+#define RS485_TX_PIN  27
+
 void setup() {
 }
 
